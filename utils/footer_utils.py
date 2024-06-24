d55e1059bfea47e9276d803c8e355aa24a5a2fbc
Andy Tillo <andy.tillo@teradata.com>
2024-06-24 12:25:45 -0700

    push 1225 exact copy from prod

--- utils/footer_utils.py
+++ utils/footer_utils.py
@@ -1,7 +1,7 @@
 # footer_utils.py  
 import logging  
   
-APP_VERSION = "1.624.1149"  
+APP_VERSION = "1.624.1225"  
 OPENAI_MODEL = "gpt4o"  
   
 def generate_footer(platform: str, response_time: float) -> str:  
