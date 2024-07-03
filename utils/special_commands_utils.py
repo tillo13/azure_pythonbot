9238fa5e857f4307972dfbccfee010630861212f
Andy Tillo <andy.tillo@teradata.com>
2024-07-03 14:48:27 -0700

    bullet point career2

--- utils/special_commands_utils.py
+++ utils/special_commands_utils.py
@@ -62,45 +62,35 @@ def extract_thread_ts(activity):
 
 async def handle_special_commands(turn_context: TurnContext) -> bool:  
     """Handle special commands starting with '$'.  
-
     Args:  
         turn_context (TurnContext): The context object for the turn.  
-
     Returns:  
         bool: True if a special command was handled, False otherwise.  
     """  
     user_message = turn_context.activity.text.strip().lower()  # Convert to lowercase  
     platform = turn_context.activity.channel_id  # Get the platform (e.g., "slack", "webchat")  
     token = os.environ.get("APPSETTING_SLACK_TOKEN")  
-
     if user_message.startswith('$'):  
         command_parts = user_message[1:].split(maxsplit=1)  # Split the command into parts  
         command = command_parts[0].lower()  # The main command (e.g., 'jira')  
-
         # Extract channel_id for logging messages  
         channel_id = extract_channel_id(turn_context.activity.conversation.id)  
-
         # Log the special command invocation  
         logging.debug(f"SPECIAL COMMAND INVOKED: {command} -- Invoking the last 5 slack messages in that thread")  
-
         # Extract thread_ts directly from the activity  
         thread_ts = extract_thread_ts(turn_context.activity) or turn_context.activity.channel_data.get('SlackMessage', {}).get('event', {}).get('ts')  
-
         if thread_ts:  
             logging.debug(f"Using thread_ts for response: {thread_ts}")  
         else:  
             logging.error("Unable to find thread_ts from the activity.")  
             thread_ts = turn_context.activity.timestamp  # Default to current message timestamp  
-
         # Add hourglass reaction  
         add_reaction_to_message(token, channel_id, thread_ts, "hourglass")  
-
         try:  
             # Handle special commands  
             if command == "test":  
                 response_text = "special test path invoked!"  
                 post_message_to_slack(token, channel_id, response_text, thread_ts=thread_ts)  
-
             elif command == "formats":  
                 formatting_message = (  
                     "*Formatting Values*:\n\n"  
@@ -111,16 +101,15 @@ async def handle_special_commands(turn_context: TurnContext) -> bool:
                     "* Code block:\n```\nthis is a code block with newline inside\n```\n\n"  
                 )  
                 post_message_to_slack(token, channel_id, formatting_message, thread_ts=thread_ts)  
-
             elif command == "help":  
                 help_message = (  
                     f"*Commands Available*:\n\n"  
                     f"*$test*: `Invokes a special test path.`\n\n"  
                     f"*$formats*: `Displays formatting values that work for Slack.`\n\n"  
                     f"*$jira <issue_key> or <JIRA URL>*: `Fetches and displays details of the specified JIRA issue.`\n\n"  
+                    f"*$person <name>*: `Searches for the specified person and displays their information.`\n\n"  
                 )  
                 post_message_to_slack(token, channel_id, help_message, thread_ts=thread_ts)  
-
             elif command == "jira" and len(command_parts) > 1:  
                 input_str = command_parts[1]  
                 issue_key = extract_jira_issue_key(input_str)  
@@ -130,7 +119,6 @@ async def handle_special_commands(turn_context: TurnContext) -> bool:
                         issue_details = await fetch_issue_details(issue_key)  
                         response_time = time.time() - start_time  
                         footer = generate_footer(platform, response_time)  
-
                         # Create Slack message with the JIRA response  
                         slack_message = create_slack_message(issue_details, footer, is_jira_response=True)  
                         post_message_to_slack(token, channel_id, slack_message['blocks'][0]['text']['text'], thread_ts=thread_ts)  
@@ -140,8 +128,6 @@ async def handle_special_commands(turn_context: TurnContext) -> bool:
                 else:  
                     invalid_key_text = "Invalid JIRA issue key or URL."  
                     post_message_to_slack(token, channel_id, invalid_key_text, thread_ts=thread_ts)  
-
-
             elif command == "person" and len(command_parts) > 1:  
                 person_name = command_parts[1]  
                 start_time = time.time()  # Start timing the response  
@@ -155,23 +141,16 @@ async def handle_special_commands(turn_context: TurnContext) -> bool:
                 except Exception as err:  
                     error_text = f"Error searching for person: {err}"  
                     post_message_to_slack(token, channel_id, error_text, thread_ts=thread_ts)  
-
-
-
             else:  
                 unknown_command_text = f"I don't understand that command: {command}"  
                 post_message_to_slack(token, channel_id, unknown_command_text, thread_ts=thread_ts)  
-
             # Remove hourglass and add greencheckmark reaction  
             remove_reaction_from_message(token, channel_id, thread_ts, "hourglass")  
             add_reaction_to_message(token, channel_id, thread_ts, "white_check_mark")  
-
         except Exception as e:  
             logging.error(f"Exception occurred while handling command: {e}")  
             post_message_to_slack(token, channel_id, f"An error occurred: {e}", thread_ts=thread_ts)  
             # Remove hourglass reaction in case of error  
             remove_reaction_from_message(token, channel_id, thread_ts, "hourglass")  
-
         return True  
-
     return False  
